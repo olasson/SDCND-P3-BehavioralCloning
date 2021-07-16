1fdd37d80861d0874dcc992a765d3012f5d347a5
olasson <oystein.lasson@gmail.com>
2021-07-16 22:03:53 +0200

    Showing images

--- behavioral_cloning.py
+++ behavioral_cloning.py
@@ -5,8 +5,8 @@ import argparse
 from os.path import join as path_join
 
 # Custom imports
-from code.misc import file_exists, folder_guard, folder_is_empty, parse_file_path
-from code.io import load_config, glob_images
+from code.misc import file_exists, folder_guard, folder_is_empty, parse_file_path, pick_triplets
+from code.io import load_config, glob_images, load_sim_log, load_images
 from code.plots import plot_images
 from code.prepare import prepare_data
 
@@ -27,8 +27,16 @@ if __name__ == "__main__":
         '--driving_log',
         type = str,
         nargs = '?',
-        default = '',
+        default = './data/driving_log.csv',
         help = 'File path to a .csv file containing the driving log from the simulator.',
+    )
+
+    # Show
+
+    parser.add_argument(
+        '--show_images',
+        action = 'store_true',
+        help = 'Show a random subset of images from the simulator.'
     )
 
 
@@ -55,6 +63,7 @@ if __name__ == "__main__":
     # Init paths
 
     file_path_config = args.model_config
+    file_path_driving_log = args.driving_log
 
     # Init config
 
@@ -64,11 +73,33 @@ if __name__ == "__main__":
 
     # Init flags
 
+    flag_show_images = args.show_images
+
     flag_force_save = args.force_save
 
     # Folder setup
 
     folder_guard(FOLDER_DATA)
+
+    if flag_show_images:
+
+        if not file_exists(file_path_driving_log):
+            print(ERROR_PREFIX + 'You are trying to show images, but the driving log at: ' + file_path_driving_log + ' does not exist!')
+            exit()
+
+        n_triplets = 3
+
+        file_paths = load_sim_log(file_path_driving_log)[1]
+        file_paths = pick_triplets(file_paths, n_triplets, dtype = 'U128')
+
+        images, file_names = load_images(file_paths)
+
+        titles = n_triplets * ['Left', 'Center', 'Right']
+        title_fig_window = 'images_driving_log'
+
+        plot_images(images, titles, title_fig_window)
+
+
 
     if model_config is not None:
         print(INFO_PREFIX + 'Using config from: ' + file_path_config)
