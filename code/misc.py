13fe42e7ca9b1b0a9fcb1a589cfd9d32e2293dc5
olasson <oystein.lasson@gmail.com>
2021-07-17 10:23:06 +0200

    Added function for file type checking

--- code/misc.py
+++ code/misc.py
@@ -3,7 +3,9 @@ This file contains some miscellaneous helper functions and os wrappers.
 """
 
 import os
+import numpy as np
 
+N_CAMS = 3
 
 def file_exists(file_path):
     """
@@ -94,5 +96,64 @@ def parse_file_path(file_path):
 
     return folder_path, file_name
 
+
+def pick_triplets(data, n_triplets, dtype = np.float32):
+    """
+    Pick out sets of 'cam triplets'  (left, center, right). 
+    
+    Inputs
+    ----------
+    data: numpy.ndarray
+        Numpy array containing scalar values.
+    n_triplets: int
+        The number of triplets. 
+       
+    Outputs
+    -------
+    data_out: numpy.ndarray
+        Numpy array containing a set of 'n_triplets'.
+        
+    """
+
+    n_samples = n_triplets * N_CAMS
+
+    data_out = np.zeros((n_samples), dtype = dtype)
+
+    indices = range(0, len(data), N_CAMS)
+
+    for i in range(0, n_samples, N_CAMS):
+        k = np.random.choice(indices)
+        data_out[i] = data[k + 1] # Left
+        data_out[i + 1] = data[k] # Center
+        data_out[i + 2] = data[k + 2] # Right
+
+    return data_out
+
+
+def is_file_type(file_path, file_type):
+    """
+    Check if a file has the expected file type or not.
+    
+    Inputs
+    ----------
+    file_path : str
+        Path to a file.
+    file_type : str
+        File type extension.
+       
+    Outputs
+    -------
+    bool
+        True if file exists and is of type 'file_type', false otherwise.
+        
+    """
+
+    if not file_exists(file_path):
+        return False
+
+    if file_path.endswith(file_type):
+        return True
+
+    return False
 
 
